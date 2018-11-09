4bc10bf5671d7f0e7a1bba436f551814576e39c4
aubrin cole <dylan_colwell@davincischools.org>
2018-11-09 15:30:37 -0800

    Update Donald.py

--- Donald.py
+++ Donald.py
@@ -4,28 +4,46 @@ from gopigo import *	#Has the basic functions for controlling the GoPiGo Robot
 import sys	#Used for closing the running program
 print ("This is a basic example for the GoPiGo Robot control")
 print ("Press:\n\tp: begin dance \n\tx: Stop GoPiGo Robot\n\tz: Exit\n")
-set_left_motor(200)
-set_right_motot(200)
+set_left_speed(200)
+set_right_speed(200)
 def chorus():
     fwd()
-    time.sleep(0.05)
+    print("chorus")
     time.sleep(2)
     bwd()
-    print ("working")
+    print ("back")
     time.sleep(1)		
     fwd()
+    print("forward")
     time.sleep(.5)
-    print("working")
+    print("left")
     left()
     time.sleep(1)
-    enc_tgt(1,1,36) #one full rotation left		
+    enc_tgt(1,1,36)
+    print("one full rotation left")		
     left()
+    print("left")
     time.sleep(1)
-    print("working")
+    print("chorus end")
+def fun():
+    fwd()
+    print ("fun")
+    time.sleep(1.5)
+    bwd()
+    print ("come like a weaver")
+    time.sleep(1)
+    left()
+    print ("swing")
+    time.sleep(1)
+    right()
+    print ("sway")
+    time.sleep(1)
+    print ("come like a tailor")
+
 while True:
 	print ("Enter the Command:"),
 	a=raw_input()	# Fetch the input from the terminal
-	
+
 	if a=='p':
 	    print("Donald MacGillivray")
 	    right()
@@ -33,30 +51,62 @@ while True:
 	    chorus()
 	    print ("working")
 	    right()
+	    print("right")
+	    time.sleep(10)
+	    chorus()
+	    right()
+	    print("right")
+	    time.sleep(1)
+	    fun()
+	    fwd()
+	    print ("forward")
+	    time.sleep(3)
+	    bwd()
+	    print ("backwards")
+	    time.sleep(3)
+	    print("round 2")
+	    right()
+	    time.sleep(10)
+	    chorus()
+	    print ("working")
+	    right()
 	    time.sleep(10)
 	    print("working")
-	    chorus
+	    chorus()
+	    right()
+	    print("right")
+	    time.sleep(1)
+	    fun()
+	    fwd()
+	    print ("forward")
+	    time.sleep(3)
+	    bwd()
+	    print ("backwards")
+	    time.sleep(3)
 	    set_left_speed(0)
 	    set_right_speed(0)
-	    speed=20
-	    right()
+	    speed=100
 	    while speed <=255:
-	        
+
 	        set_left_speed(0)
 	        set_right_speed(speed)
 	        speed = speed + 10
-	        time.sleep(0.01)
+	        time.sleep(0.1)
 	        print("working")
 	    set_left_speed(255)
 	    set_right_speed(255)
-	    set
 	    chorus()
-	   
+	    fun()
+	    chorus()
+	    fun()
+	    chorus()
+	    chorus()
 	
 	elif a=='x':
 		stop()	# Stop
 	elif a=='t':
-		increase_speed()	# Increase speed
+		fun()
+	# Increase speed
 	elif a=='g':
 		decrease_speed()	# Decrease speed
 	elif a=='z':
